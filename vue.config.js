d4f04b45035d1e86ac9ce655f5f9f7993ff64475
richardpbCC <rpbeavis@gmail.com>
2022-01-30 19:10:07 +0900

    refactor: scripts for production and development

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
       proxy: {
         "/api": {
-          target: "http://localhost:9000",
+          target: "http://localhost:4000",
           secure: false,
         },
       },
